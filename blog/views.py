691fd1a225bd849df3f61e7df5a70d22fc6f8fa8
susruth08 <susruth08@gmail.com>
2019-11-04 12:40:21 +0530

    add styling

--- blog/views.py
+++ blog/views.py
@@ -7,12 +7,13 @@ from posts.models import Post
 from responses.models import Response
 from comments.models import Comment
 from django.db.models import Max
+from django.contrib import messages
 
 
 
 def index(request):
-
     context = {}
+    #shows all posts along with the author
     if request.user.is_authenticated:
         posts = Post.objects.all()
         context = {"user": request.user, "posts": posts }
@@ -23,6 +24,7 @@ def index(request):
     return render(request, 'blog/index.html', context)
 
 def login(request):
+    #check for loginn if user exists log in else return error message
     if request.method == "POST":
         username = request.POST['username']
         password = request.POST['password']
@@ -31,6 +33,7 @@ def login(request):
             auth_login(request, user)
             return redirect('index')
         else:
+            messages.warning(request, 'Invalid Login')
             return render(request, 'blog/login.html')
     else:
         context = {}
@@ -39,14 +42,21 @@ def login(request):
 def register(request):
     context = {}
     print("register")
+    #register user for uniuq values of username and email
     if request.method == "POST":
         username = request.POST['username']
         email = request.POST['email']
         password = request.POST['password']
         password2 = request.POST['password2']
-        user = User.objects.create_user(username, email, password)
-        user.save()
-
+        if User.objects.filter(username=username).count() > 0:
+            messages.warning(request, 'Username taken')
+        elif User.objects.filter(email=email).count() > 0:
+            messages.warning(request, 'Email taken')
+        elif password != password2:
+            messages.warning(request, 'Passwords Do Not Match')
+        else:
+            user = User.objects.create_user(username, email, password)
+            user.save()
     return render(request, 'blog/register.html', context)
 
 def log_out(request):
@@ -75,15 +85,14 @@ def analytics(request):
             max_likes_post = max(likes,key=likes.count)
             max_likes_post = Post.objects.get(id=max_likes_post)
         else:
-            max_likes_post =0
-
+            max_likes_post =None
         dislikes = Response.objects.filter(post__in=posts, liked=False, disliked=True).values('post')
         dislikes = [dislike['post'] for dislike in dislikes]
         if len(dislikes) > 0:
             max_dislikes_post = max(dislikes,key=dislikes.count)
             max_dislikes_post = Post.objects.get(id=max_dislikes_post)
         else:
-            max_dislikes_post = 0
+            max_dislikes_post = None
 
         comments = Comment.objects.filter(post__in=posts).values('post')
         comments = [c['post'] for c in comments]
@@ -91,9 +100,14 @@ def analytics(request):
             max_comments_post = max(comments,key=comments.count)
             max_comments_post = Post.objects.get(id=max_comments_post)
         else:
-            max_comments_post=0
-        id = Post.objects.filter(author=request.user).values('views', 'id').annotate(Max('views')).first()['id']
-        max_views_post = Post.objects.get(id=id)
+            max_comments_post=None
+
+        if posts:
+            id = posts.values('views', 'id').annotate(Max('views')).first()['id']
+            max_views_post = Post.objects.get(id=id)
+        else:
+            max_views_post = None
+
 
 
         context = {
